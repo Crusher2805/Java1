06b5d3f769d3ecc12fb686b8ff79451bd1c37fc7
Crusher2805 <151662737+Crusher2805@users.noreply.github.com>
2024-06-05 20:43:29 -0600

    Add files via upload

---
+++ src/javaapplication1/JavaApplication2.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication2;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Aulas Heredia
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /* Ejercicio #1
+        for (int i = 5; i < 10; i++) {
+            System.out.println("@@@@@@@@@@");
+            
+        }
+
+         */
+        
+        /* Ejercicio #2
+        int tablas = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el numero de la tabla de multiplicar"));
+        for (int i = 1; i <= 10; i++) {
+        int resultado = tablas * i;  
+        JOptionPane.showMessageDialog(null, " La tabla de multiplicar del numero " + tablas + " es " + resultado);
+        }
+        */
+    
+    String triangulo = JOptionPane.showInputDialog(null, "Ingresa el numero de filas del triangulo que desea");
+    
+    
+    }
+}
