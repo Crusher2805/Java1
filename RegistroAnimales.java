0cd884f4f889349290479a525116b31079a527e4
Crusher2805 <151662737+Crusher2805@users.noreply.github.com>
2024-06-28 08:39:33 -0600

    Add files via upload

---
+++ RegistroAnimales.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoprograbasica;
+
+public class RegistroAnimales {
+    public String nombre; 
+    public String Especie;
+    public int edad;
+    public String habitat;
+}
